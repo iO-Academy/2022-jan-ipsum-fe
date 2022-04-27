957457f88633b283cf19a7728722bd875d989ecb
Grace Palacz <gracelizbuckley@gmail.com>
2022-04-27 12:27:27 +0100

    addeeed input, with plus and minus buttons and generate button plus styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './normalize.css'
 import Footer from "./Footer/Footer";
 import Title from "./Title/Title";
 import About from "./About/About";
+import ParaGenerate from "./ParaGenerate/ParaGenerate";
 
 function App() {
   return (
       <>
         <Title />
         <About />
+        <ParaGenerate />
         <Footer />
       </>
   );
---
+++ src/ParaGenerate/ParaGenerate.js
@@ -0,0 +1,41 @@
+import {useState} from "react";
+
+function ParaGenerate() {
+
+    const [inputValue, setInputValue] = useState(1)
+
+    const plusButtonFunction = () => {
+        let number = inputValue +1
+        if (number > 10) {
+            number = 10
+        }
+        setInputValue(number)
+    }
+
+    const minusButtonFunction = () => {
+        let number = inputValue -1
+        if (number < 1) {
+            number = 1
+        }
+        setInputValue(number)
+    }
+    
+
+    return (
+        <>
+        <h6 className="formTitle">How many paragraphs do you need?</h6>
+        <div className='formSection'>
+            <div className="inputSection">
+                <input type="text" value={inputValue} max="10" min="1" className="numInput" />
+                <input type="button" value="+" className="plusButton" onClick={plusButtonFunction} />
+                <input type="button" value="-" className="minusButton" onClick={minusButtonFunction} />
+            </div>
+            <div className="generateButton">
+                <input type="submit" value="Generate" />
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default ParaGenerate;
